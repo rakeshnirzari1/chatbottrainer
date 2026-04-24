b33957b0fdfcea4944c36b0e84744a57f42093e6
rakeshnirzari1 <124740209+rakeshnirzari1@users.noreply.github.com>
2026-04-24 12:55:49 +1000

    Updated App.tsx

--- src/pages/LoginPage.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { LoginForm } from '../components/auth/LoginForm';
-
-export function LoginPage() {
-  return <LoginForm />;
-}
--- src/pages/SignupPage.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { SignupForm } from '../components/auth/SignupForm';
-
-export function SignupPage() {
-  return <SignupForm />;
-}
